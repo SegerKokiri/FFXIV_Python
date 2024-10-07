1366194bea813049e11fe0b002128e5ae765db8b
Blake <Blake@HyruleCastle.xyz>
2024-10-06 20:31:57 -0700

    Added a function if type is wanted Added if statement for character or mount/minion Fixed spacing a bit

--- FFXIV_Collect.py
+++ FFXIV_Collect.py
@@ -1,61 +1,86 @@
-#Simple tool to just copy some stuff from FFXIV  d
+#Simple tool to just copy some stuff from FFXIV 
                                          
 import requests
 import json
 import pandas as pd
 
+#Sample character ID 2277896
+
 api_url = "https://ffxivcollect.com/api/"
 
 category_list = [
-    'achievements', 'titles', 'mounts', 'minions', 'orchestrions', 'frames', 
-    'spells', 'emotes', 'bardings', 'hairstyles', 'armoires', 'fashions', 
-    'triad', 'records', 'survey records', 'leves', 'relics', 'tomestones', 
-    'characters'
+    'mounts', 'minions', 'character info'
 ]
 
 cat_df = pd.DataFrame(category_list, columns=['Category'])
 cat_df.index += 1
 
 print('''
-      Welcome to FFXIV Collect, simple version!/n
-      Below is a catagory list of collectables from FFXIV/n
+      Welcome to FFXIV Collect, simple version!\n
+      Below is a catagory list of collectables from FFXIV
       ''')
 print(cat_df)
-# cat_input = int(input('Go ahead and input the number associated with the category you\'d like to see more about:'))
-cat_pick = cat_df.loc[3, 'Category']
-print(f'You have selected {cat_pick}')
+cat_input = int(input('Go ahead and input the number associated with the category you\'d like to see more about: '))
+cat_pick = cat_df.loc[cat_input, 'Category']
+print(f'\nYou have selected {cat_pick}\n')
 
 cat_url = api_url + cat_pick
 
 #This code is going to be used to pull all items from that category
 response = requests.get(f'{cat_url}')
 data = response.json()
+choice = 0
 
-choice = 2 #int(input('Input 1 if you want to search mounts by source type and 2 for a top 10 list:'))
-
-
-#This is used to show mounts from specific source types
-if choice == 1:
+def search_item(type):
     source_type_mount = []
     for result in data['results']:
         for source in result['sources']:
-            if source['type'] == 'Raid':
-                source_type_mount.append(result) 
-#mount_df = pd.DataFrame(data['results'])
-#print(data['count'])
-    # stm_df = pd.DataFrame(source_type_mount)
-#print(mount_df[['name']])
-#print(stm_df['name'])
+            if source['type'] == type:
+                source_type_mount.append(result)
+    mount_df = pd.DataFrame(data['results'])
+    print(f'Total mounts that are from source {type} is {len(source_type_mount)}\n')
+    stm_df = pd.DataFrame(source_type_mount)
+    #print(mount_df[['name']])
+    stm_df.index += 1
+    print(stm_df['name'])
+
+#If character info is selected
+if cat_input == 3:
+    print('Character stuff')
+
+else:
+    type_list=[]
+    for result in data['results']:
+        for source in result['sources']:
+            if source['type'] not in type_list:
+                type_list.append(source['type'])
+    type_list_df = pd.DataFrame(type_list, columns=['Name'])
+    type_list_df.index += 1
+
+    choice = int(input(f'Input 1 if you want to search {cat_pick} by source type and 2 for a top 10 list: '))
+
+
+#This is used to show mounts from specific source types
+
+    if choice == 1:
+        print(type_list_df)
+        source_type = int(input('\nWhat source would you like to search? '))
+        source_type -= 1
+        search_item(type_list[source_type])
 
 #Code to sort items with the most/least amount of owners
-elif choice == 2:
-    Most_Least = 'most' #input('Would you like to see the top 10 most or least owned mounts?')
-    top_list = []
-    if Most_Least == 'most':
+    elif choice == 2:
+        Most_Least = input(f'Would you like to see the top 10 most or least owned {cat_pick}? ')
+        top_list = []
+        a_or_d = False
+        if Most_Least == 'most':
+            a_or_d = True
         for results in data['results']:
             top_list.append({'name': results['name'], 'percentage owned':float(results['owned'].replace('%',''))})
     
     #print(top_list)
-    sorted_top_list = sorted(top_list, key=lambda x: x['owned'], reverse=True)
-    sorted_top_list_df = pd.DataFrame(sorted_top_list)
-    print(sorted_top_list_df.head(10))
+        sorted_top_list = sorted(top_list, key=lambda x: x['percentage owned'], reverse=a_or_d)
+        sorted_top_list_df = pd.DataFrame(sorted_top_list)
+        sorted_top_list_df.index += 1
+        print(sorted_top_list_df.head(10))
+    
